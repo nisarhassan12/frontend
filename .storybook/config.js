e3a82e0e907e8d78fd4d617194ea5731cf8e01f9
v1rtl <pilll.PL22@gmail.com>
2019-09-17 23:10:20 +0300

    remove storybook

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react'
-
-// automatically import all files ending in *.stories.js
-const req = require.context('./stories', true, /\.stories\.js$/)
-
-configure(() => {
-  req.keys().forEach(filename => req(filename))
-}, module)
